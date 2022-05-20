7baf685939c767e15f5c49c6abbf465e47e4c99b
DrAlexWild <ricardogoncalves126@gmail.com>
2022-05-20 22:38:01 +0100

    fix heroku new model

--- portfolio/admin.py
+++ portfolio/admin.py
@@ -8,14 +8,14 @@ from .models import Class
 from .models import Language
 from .models import Hobbie
 from .models import Quiz
+from .models import Project_image_description
 
 
 admin.site.register(Blog_Post)
-
 admin.site.register(Professor)
 admin.site.register(Class)
 admin.site.register(Project)
 admin.site.register(Language)
 admin.site.register(Hobbie)
-
 admin.site.register(Quiz)
+admin.site.register(Project_image_description)
